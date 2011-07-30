ee7b61659a83d84f623f3b4e040cd7fbf0da90e3
Evan Hazlett <ejhazlett@gmail.com>
2011-07-30 04:50:30 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import sys
+
+REQUIRES = []
+
+setup(name='payments',
+    version = '0.01',
+    author = 'Evan Hazlett',
+    author_email = 'ejhazlett@gmail.com',
+    packages = ['payments'],
+    description = 'Library for handling online payments',
+    url = 'http://github.com/ehazlett/payments',
+    license = 'License :: OSI Approved :: Apache Software License',
+    long_description = """
+    This is a wrapper library that helps in working with online payment systems
+    like PayPal.
+    """,
+    download_url = 'https://github.com/ehazlett/payments/tarball/master',
+    install_requires = REQUIRES,
+    platforms = [
+        "All",
+        ],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        ]
+    )
+
